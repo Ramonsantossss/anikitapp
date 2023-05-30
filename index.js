71d08be1ee8ac99d225e6e9e3d1a24ac5c48df88
Clovermods <111981866+CloverMods@users.noreply.github.com>
2023-05-30 15:14:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-
-var favicon = require('serve-favicon');
+__path = process.cwd()
 var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
-var router = express.Router();
-//var mainrouter = require('./routes/main'),
-//    apirouter = require('./routes/api')
+
 
 var app = express()
+var mainrouter = require('./main')
 app.enable('trust proxy');
 app.set("json spaces",2)
 app.use(cors())
 app.use(secure)
-//app.use(favicon(__path +'/views/favicon.ico'))
 app.use(express.static("public"))
 
-
-router.get('/', (req, res) => {
-    res.sendFile(__path + '/paginas/index.html')
-})
+app.use('/', mainrouter);
 
 app.listen(8080, () => {
     console.log("Server running on port 8080")
