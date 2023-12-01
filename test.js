9a3b4cf4099573e78576b0c1800a7906744203d0
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-12-01 07:39:14 -0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const apiModule = require('trevo-api');
+//const apiModule = require('trevo-api');
 
 async function verificaImagensCapitulo(url) {
     try {
